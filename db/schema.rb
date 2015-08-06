a8b17ebee0413c1f3e0673836859b5948b9add8b
Kenny Ope <k.ope22@yahoo.com>
2015-08-06 15:39:17 -0400

    User Profiles still needs work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723204310) do
+ActiveRecord::Schema.define(version: 20150728172917) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -93,6 +93,24 @@ ActiveRecord::Schema.define(version: 20150723204310) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "cid"
+    t.date     "birthday"
+    t.string   "sex"
+    t.string   "tel"
+    t.string   "address"
+    t.string   "tagline"
+    t.text     "introduction"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "simple_hashtag_hashtaggings", force: :cascade do |t|
     t.integer "hashtag_id"
