0005da6cf2b7e6d9c4d6125fac8fdff171e04203
Alona King <alonaking13@gmail.com>
2015-08-10 21:54:05 -0700

    liking posts/comments works. Still building better UI/UX for the like button and the post index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807212653) do
+ActiveRecord::Schema.define(version: 20150810171255) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
